88c47d7c70527619af2bbf27a2e83d4daac5d6d3
apparao432@gmail.com <apparao.n@autorabit.com>
2019-05-14 13:37:01 +0530

    Commit From AutoRABIT [Branch Baseline (Clean Branch)] [LabelName:15212Seven]

--- src/lwc/helloWorld/helloWorld.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-export default class HelloWorld extends LightningElement {
-    changeHandler(event) {
-        this.greeting = event.target.value;
-    }
-}
